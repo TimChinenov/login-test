a0975aa1913ce979dc156d6e82f7f63a5b6659b4
Tim Chinenov <tim.chinenov@gmail.com>
2023-02-10 20:30:17 -0800

    first init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('')
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <input
+          type="text"
+          placeholder='Username'
+          onBlur={(event) => setUsername(event.target.value)}>
+        </input>
+
+        <input
+          type="password"
+          placeholder='Password'
+          onBlur={(event) => setPassword(event.target.value)}>
+        </input>
+
+        <div>
+          <button onClick={() => createAccount(username, password)} >Login</button>
+          <button onClick={() => login(username, password)}>Create Account</button>
+        </div>
+      </div>
     </div>
   );
+}
+
+function createAccount(username: string, password: string) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username: username, password: password})
+  }
+
+  fetch('http://localhost:8080/users', requestOptions)
+    .then(response => response.json())
+}
+
+function login(username: string, password: string) {
+
 }
 
 export default App;
